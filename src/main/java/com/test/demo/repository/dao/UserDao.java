1d596d6835868cc174dffc69b93016a50e66cbde
yangliuxin <yangliuxin@xin.com>
2018-12-05 18:04:10 +0800

    fix bugs

--- src/main/java/com/test/demo/repository/dao/UserDao.java
+++ src/main/java/com/test/demo/repository/dao/UserDao.java
@@ -4,7 +4,6 @@ import com.test.demo.domain.User;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 import java.util.List;
 
