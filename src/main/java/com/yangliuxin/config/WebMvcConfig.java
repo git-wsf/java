c2d1a37b8cb611603bb1644d639981494d81d05d
yangliuxin <yangliuxin@xin.com>
2018-12-20 21:24:29 +0800

    over

--- src/main/java/com/yangliuxin/config/WebMvcConfig.java
+++ src/main/java/com/yangliuxin/config/WebMvcConfig.java
@@ -27,6 +27,13 @@ public abstract class WebMvcConfig implements WebMvcConfigurer {
                         .allowCredentials(true)
                         .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
                         .maxAge(3600);
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "OPTIONS", "PUT")
+//                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+//                                "Access-Control-Request-Headers")
+//                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+//                        .allowCredentials(true).maxAge(3600);
             }
         };
     }
