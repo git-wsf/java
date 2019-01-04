b2a453d999d867ef5c0bbe884985cb560883d0a7
yangliuxin <yangliuxin@xin.com>
2019-01-04 10:20:19 +0800

    excel

--- src/main/java/com/yangliuxin/controller/wechat/WeChatController.java
+++ src/main/java/com/yangliuxin/controller/wechat/WeChatController.java
@@ -783,21 +783,21 @@ public class WeChatController {
 
 class MyX509TrustManager implements X509TrustManager {
 
- @Override
- public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-     // TODO Auto-generated method stub
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        // TODO Auto-generated method stub
 
- }
+    }
 
- @Override
- public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-     // TODO Auto-generated method stub
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        // TODO Auto-generated method stub
 
- }
+    }
 
- @Override
- public X509Certificate[] getAcceptedIssuers() {
-     // TODO Auto-generated method stub
-     return null;
- }
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
